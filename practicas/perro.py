ecf98fd58d639bf6221f8252e6f1fff7f41a4a5a
teodulonunez <teodulo.nunez19@gmail.com>
2025-10-17 14:22:05 -0400

    carpeta

--- practicas/perro.py
+++ practicas/perro.py
@@ -5,18 +5,24 @@ class Perro:
         color1 = ("marron", "negro", "blanco", "amarillo")
         color2 = ("marron", "negro", "blanco", "amarillo")
         color_base = random.choice(color1)
-        color_seg = random.choice(color2)
+        color_seg  = random.choice(color2)
         
         tamano_op = ("pequeño", "mediano", "grande")
-        tamano = random.choice(tamano_op)
+        tamano    = random.choice(tamano_op)
 
         pelo_op = ("corto", "largo")
-        pelo = random.choice(pelo_op)
+        pelo    = random.choice(pelo_op)
 
         if color_base != color_seg:
             manchas = color_seg
             print("Color base",color_base, "manchas", color_seg, "tamaño", tamano)
         else:
             print("Color base",color_base, "SIN manchas", "tamaño", tamano)
+
+    def ladrar():
+        print("GUAU")
+
+        
 #perro = Perro(input("Introduce: "))
+perro1 = Perro()
 perro2 = Perro()
