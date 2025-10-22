952ee941fc22595c849ef6cc14f86285a49fb32a
teodulonunez <teodulo.nunez19@gmail.com>
2025-10-22 15:34:42 -0400

    camio de nombre

---
+++ practicas/guardian.py
@@ -0,0 +1,29 @@
+from perro import Perro
+import random
+
+class Guardian (Perro):
+    def __init__(self):
+        self.estado = True
+        self.color1 = ("marron", "negro", "griz")
+        self.color_base = random.choice(self.color1)
+        self.tamano = "grande"
+        self.pelo_op = "largo"
+        
+        print("guardian de color",self.color_base,"tamaño",self.tamano, "pelo:",self.pelo_op)
+
+    def vigilar(self):
+        if self.verificar_estado():
+            print("vigilando")
+        return True
+    
+    def patrullar(self):
+        if self.vigilar() and self.correr():
+            print("patrullando")
+
+
+guardian = Guardian()
+guardian.ladrar()
+print("estado",guardian.estado)
+guardian.vigilar()
+guardian.dormir()
+guardian.patrullar()
--- practicas/montana.py
+++
@@ -1,9 +0,0 @@
-from perro import Perro
-
-class Montana (Perro):
-    def __init__(self):
-        super().__init__()
-
-
-montana1 = Montana()
-montana1.comer()
