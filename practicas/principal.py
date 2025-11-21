4a0b5c5ad996c29398ed98f7c12fc966f7ba11aa
teodulonunez <teodulo.nunez19@gmail.com>
2025-11-21 10:50:52 -0400

    fusion

--- practicas/principal.py
+++ practicas/principal.py
@@ -24,24 +24,31 @@ class Principal():
 
 
     def crear_perro(self):
-        while True:
+        salir = False
+        while not salir:
             try:
                 perro1 = int(input("Elije primer perro: "))
                 if 1 <= perro1 <= 5:
-                    return perro1
+                    while True:
+                        try:
+                            perro2 = int(input("Elije segundo perro: "))
+                            if (1 <= perro2 <= 5):
+                                if perro2 != perro1:
+                                    salir = True
+                                    break
+                                else:
+                                    print(f"perros identicos perro1: {perro1} y perro2 {perro2}")
+                            else:
+                                print("opcion de 1 a 5")
+                        except ValueError:
+                                print("2excep de 1 - 5")
                 else:
                     print("opcion de 1 a 5")
             except ValueError: 
                 print("Debe ser un número de 1 a 5 " )
 
-
-
-        # validar(self, perro1)
-        # perro2 = input("Elije segundo perro:")
-
-
+        print(f"Fuera del ciclo perro1: {perro1} perro2: {perro2}")
 
 p = Principal()
-#p.mostrar_lista()
 p.mostrar_lista()
 p.crear_perro()
