f2b7351e448cb36af7be4d7ec40527cb6102bdba
teodulonunez <teodulo.nunez19@gmail.com>
2025-11-26 15:48:53 -0400

    ll

--- conexion.py
+++ conexion.py
@@ -1,30 +1,33 @@
 import psycopg2
-
 # class conectar_postrgres: #no usa parentesis por que son opcionales cuando no se hereda
+class Conectar_postgres:
+    def __init__(self):
+        #las variables aca deben tener self para ser accesibles al resto de la clase sin instanciar 
+        try:
+            self.conexion = psycopg2.connect(
+                host = "wazuh-server.cm.com.ve",
+                database = "siis",
+                user = "siis",
+                password = "siis"
+            )
+        
+            self.cursor = self.conexion.cursor()
+            self.cursor.execute("SELECT version();")
+            self.version = self.cursor.fetchone()
 
-#     def __init__(self, host, database, usuario, password):
-#         self.conn_params
+            print("Conexion ok Version: ", self.version)
+        
+        except Exception as e:
+            print("ERROR",e)
+
+    # def consultar(self, query):
+    #     resp = self.cursor.execute(query)
+        
 
 
-try:
-    conexion = psycopg2.connect(
-        host = "wazuh-server.cm.com.ve",
-        database = "siis",
-        user = "siis",
-        password = "siis"
-    )
-
-    print("Conexion OK")
-    
-    cursor = conexion.cursor()
-    cursor.execute("SELECT version();")
-    version = cursor.fetchone()
-    select = cursor.execute("SELECT * FROM razas;")
-    consulta = cursor.fetchall()
-    for consulta_resp in consulta:
-        print(f"select:")
-        print(consulta_resp)
-    print("Version: ", version)
-  
-except Exception as e:
-    print("ERROR",e)
+conn = Conectar_postgres()
+    # select = cursor.execute("SELECT * FROM razas;")
+    # consulta = cursor.fetchall()
+    # for consulta_resp in consulta:
+    #     print(f"select:")
+    #     print(consulta_resp)
